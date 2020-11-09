c3964eafbf6fddf6772b368d62df7447372c0981
rileymcallister <65777911+rileymcallister@users.noreply.github.com>
2020-11-09 13:47:13 -0600

    Uploaded part 1

---
+++ Coursera_Capstone_Project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Riley McAllister - IBM Data Science Professional Certification Capstone Project
+
+# !pip install pandas
+# !pip install numpy
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
